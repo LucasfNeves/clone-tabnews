fc2ee6d3aaa63a19d187b7985b04515c03ec6a1a
Lucas Farias <lucasfarias.ln@gmail.com>
2026-06-30 21:38:47 -0300

    feat: implement PostgreSQL database connection and update services with Docker Compose

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
+import database from "../../../../infra/database"
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result)
   response.status(200).send('A samira e muito chata')
 }
