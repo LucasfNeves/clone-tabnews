a2db826b7d1a92f2c324be41727f230759d1f033
Lucas Farias <lucasfarias.ln@gmail.com>
2026-07-10 22:46:28 -0300

    feat: enable SSL for secure database connections

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryText, params) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true, // Enable SSL for secure connections
   });
 
   try {
