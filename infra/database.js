f19015dba54ee8051ac1be93273bfe6381edc03c
Lucas Farias <lucasfarias.ln@gmail.com>
2026-07-10 23:19:55 -0300

    feat: refactor SSL configuration for database connections to use getSSLValues function

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,7 @@ async function query(queryText, params) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl:
-      process.env.NODE_ENV === "development"
-        ? false
-        : { ca: process.env.POSTGRES_CA_CERT, rejectUnauthorized: true },
+    ssl: getSSLValues(),
   });
 
   try {
@@ -30,3 +27,17 @@ async function query(queryText, params) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA_CERT_BASE64) {
+    return {
+      ca: Buffer.from(process.env.POSTGRES_CA_CERT_BASE64, "base64").toString(
+        "utf8",
+      ),
+      rejectUnauthorized:
+        process.env.POSTGRES_SSL_REJECT_UNAUTHORIZED !== "false",
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
